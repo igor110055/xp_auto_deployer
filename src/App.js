dfa36f985a5c583bce1101bede97703073a600e0
Or Kilim <orkilim@Ors-MacBook-Pro.local>
2022-05-17 11:52:43 +0300

    able to switch networks after choosing from autocomplete, and able to add networks with manually added rpc url

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import bridgenftjson from './abis/bridgenft.json'
 const contracts = ["xpnft", "bridgenft"]
 function App() {
 
-  //const providerUrl = "http://127.0.0.1:8545"
-
   const [contract, setContract] = useState(xpnftjson)
   const [contractName, setContractName] = useState("xpnft")
   //const [chainName, setChainName] = useState("")
@@ -22,6 +20,10 @@ function App() {
   const [tokenSymbol, setTokenSymbol] = useState("")
   const [baseURI, setBaseURI] = useState("")
   const [chainNames, setChainNames] = useState([])
+  const [RpcURL, setRpcUrl] = useState("")
+  const [txtVisible, setTXTVisible] = useState("none")
+  const [autoCompleteVis, setAutoCompleteVis] = useState("block")
+  const [buttonVis, setButtonVis] = useState("none")
 
 
   useEffect(() => {
@@ -29,9 +31,9 @@ function App() {
     const getChainNames = () => {
       const chainNames = chains.map((item, index) => {
         return ({
-            label: item.name,
-            id: item.chainId
-          })
+          label: item.name,
+          id: item.chainId
+        })
       })
       setChainNames(chainNames)
     }
@@ -41,58 +43,42 @@ function App() {
   }, [])
 
 
-
-
-
-  const handleSubmit = async (event) => {
-    try{
-    event.preventDefault()
-    console.log("deploying your contract")
-
-    let provider = window.ethereum
-
-    const accounts = await provider.request({ method: "eth_requestAccounts" })
-
+  const getNetworkDetails = (chainName) => {
     let i = 0;
     let network;
     for (; i < chains.length; i++) {
       if ((chains[i]).name == chain.label) {
         network = chains[i]
+        break
       }
     }
-
-    console.log("network is: ", network)
-
-    const web3 = new Web3(network.rpc[0])
-
-    //const rpcUrl = "http://127.0.0.1:8545" //((chains[i])["url"])[0]
-    const abi = contract.abi
-
-    //console.log("rpcUrl is: ", rpcUrl)
-    console.log("abi is: ", abi)
-
-
-
-    const myContract = new web3.eth.Contract(abi)
-
-    console.log("contract is: ", myContract)
-
-    //console.log(myContract)
-
-    console.log("accounts[0] is: ", accounts[0])
-
-    const result = await myContract.deploy({ data: contract.data.bytecode.object, arguments: [tokenName, tokenSymbol, baseURI] })
-      .send({ from: accounts[0] })
-
-    console.log("my result is: ", result)
-
-    console.log("contract deployed to:", result._address);
-    setMsg(`contract deployed to: ${result._address} `)
-
-  }catch(error){
-    console.log("error in handleSubmit is: ",error)
-    setMsg("error in handleSubmit is: ",error)
+    return network
   }
+
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault()
+      console.log("deploying your contract")
+
+      const provider = window.ethereum
+      const accounts = await provider.request({ method: "eth_requestAccounts" })
+      const network = getNetworkDetails(chain)
+      console.log("network is: ", network)
+      const web3 = new Web3(provider)
+      const abi = contract.abi
+      const myContract = new web3.eth.Contract(abi)
+
+      const result = await myContract.deploy({ data: contract.data.bytecode.object, arguments: [tokenName, tokenSymbol, baseURI] })
+        .send({ from: accounts[0] })
+
+      console.log("contract deployed to:", result._address);
+      setMsg(`contract deployed to: ${result._address} `)
+
+    } catch (error) {
+      console.log("error in handleSubmit is: ", error)
+      setMsg("error in handleSubmit is: ", error)
+    }
   }
 
   const changeContract = (name) => {
@@ -109,31 +95,85 @@ function App() {
   }
 
   //#region Event listeners
-  window.ethereum.on('chainChanged', () => {
-    window.location.reload();
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on('chainChanged', () => {
+        window.location.reload();
+      })
+      window.ethereum.on('accountsChanged', () => {
+        window.location.reload();
+      })
+    }
   })
-  window.ethereum.on('accountsChanged', () => {
-    window.location.reload();
-  })
-  //#endregion
 
+  const changeNetwork = async () => {
+    if (chain != "") {
+
+      //wallet_addEthereumChain
+
+      const chainDetails = getNetworkDetails(chain)
+      const hexChainId = Web3.utils.numberToHex(chainDetails.chainId)
+      console.log("new chain id is: ", hexChainId)
+      try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: hexChainId }], // chainId must be in hexadecimal numbers
+        });
+      } catch (error) {
+        if (txtVisible == "none") {
+          setTXTVisible("inline")
+          setAutoCompleteVis("none")
+          setButtonVis("block")
+          console.log("reloading...")
+
+
+        }
+        /*if(txtVisible=="inline")
+        {
+          
+        }*/
+      }
+
+    }
+  }
+
+  const addNetwork = async () => {
+    const chainDetails = getNetworkDetails(chain)
+    const hexChainId = Web3.utils.numberToHex(chainDetails.chainId)
+    await window.ethereum.request({
+      method: 'wallet_addEthereumChain',
+      params: [
+        {
+          chainId: hexChainId,
+          chainName: chainDetails.name,
+          rpcUrls: [RpcURL]
+        },
+      ]
+    })
+  }
+
+  useEffect(() => {
+    changeNetwork()
+  }, [chain])
+  //#endregion
   return (
     <div className="App">
       <h1>XPNFT deployer</h1>
       <form onSubmit={handleSubmit}>
         <label>Enter token name: </label>
-        <input type={"text"} value={tokenName} placeholder="exampleToken" onChange={(e) => { setTokenName(e.target.value) }} />
+        <input type={"text"} value={tokenName} required placeholder="exampleToken" onChange={(e) => { setTokenName(e.target.value) }} />
         <br />
         <br />
         <label>Enter token symbol </label>
-        <input type={"text"} value={tokenSymbol} placeholder="SYM" onChange={(e) => { setTokenSymbol(e.target.value) }} />
+        <input type={"text"} value={tokenSymbol} required placeholder="SYM" onChange={(e) => { setTokenSymbol(e.target.value) }} />
         <br />
         <br />
         <label>Enter base URI </label>
-        <input type={"text"} value={baseURI} placeholder="http://something.com" onChange={(e) => { setBaseURI(e.target.value) }} />
+        <input type={"text"} value={baseURI} required placeholder="http://something.com" onChange={(e) => { setBaseURI(e.target.value) }} />
         <br />
         <br />
         <Autocomplete
+          style={{ display: autoCompleteVis }}
           disablePortal
           id="combo-box-demo"
           options={chainNames}
@@ -143,6 +183,17 @@ function App() {
           sx={{ width: 300 }}
           renderInput={(params) => <TextField {...params} label="chains" />}
         />
+        <div style={{ display: txtVisible }}>
+          <text >It looks like you're trying to deploy to a network you don't have in your wallet</text>
+          <br />
+          <text> please add its RPC URL and click to add the network: </text>
+          <br />
+          <input type={"text"} value={RpcURL} onChange={(e) => {setRpcUrl(e.target.value) }} />
+          <br />
+          <br />
+          <button type='button' style={{ display: buttonVis }} onClick={addNetwork} >{`Add ${chain.label} to MetaMask`}</button>
+        </div>
+        <br />
         <button type="submit" >Deploy</button>
       </form>
       <text>{addressMsg}</text>
@@ -169,7 +220,6 @@ export default App;
  */
 
 
-  /*onInputChange={(event,newValue)=>{
-    setChainName(newValue)
-  }}*/
-  
+/*onInputChange={(event,newValue)=>{
+  setChainName(newValue)
+}}*/
